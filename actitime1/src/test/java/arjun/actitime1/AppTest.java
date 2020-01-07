fcab1eab3c1913aa28e931e6baf6a99621aa8e26
Arjun <Arjun@192.168.1.7>
2020-01-07 18:32:32 +0530

    fixed

---
+++ actitime1/src/test/java/arjun/actitime1/AppTest.java
@@ -0,0 +1,12 @@
+package arjun.actitime1;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class AppTest {
+	@Test
+	public void testa() {
+		Reporter.log("fixed",true);
+	}
+
+}
